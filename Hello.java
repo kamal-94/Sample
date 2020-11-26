109375937d17541f3426aff83d69031f349fec0c
kamal-94 <75011065+kamal-94@users.noreply.github.com>
2020-11-26 21:48:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
     
-    System.out.println("Hi,this is kamal raj cloud kamal mass");
+    System.out.println("Hi,this is kamal raj");
     }
 }
